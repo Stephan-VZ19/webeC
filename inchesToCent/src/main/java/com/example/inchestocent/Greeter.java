b922513c43d564d1807c7438630930f6750b37cc
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-03-17 12:55:51 +0100

    Greeting demo, not working

--- inchesToCent/src/main/java/com/example/inchestocent/Greeter.java
+++ inchesToCent/src/main/java/com/example/inchestocent/Greeter.java
@@ -1,5 +1,8 @@
 package com.example.inchestocent;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Greeter {
 
     private final GreetingDestination dest;
--- inchesToCent/src/main/java/com/example/inchestocent/GreetingDestination.java
+++ inchesToCent/src/main/java/com/example/inchestocent/GreetingDestination.java
@@ -1,5 +1,8 @@
 package com.example.inchestocent;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class GreetingDestination {
     public void write(String greeting) {
         System.out.println(greeting);
