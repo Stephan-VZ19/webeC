ec9a84693aa9582f5b09f738bfacc7918699a453
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-03-17 12:23:56 +0100

    update

---
+++ inchesToCent/src/main/java/com/example/inchestocent/InchesToCentApplication.java
@@ -0,0 +1,13 @@
+package com.example.inchestocent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InchesToCentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InchesToCentApplication.class, args);
+    }
+
+}
