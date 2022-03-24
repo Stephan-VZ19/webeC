d1ff80c857d50b835cba7a3367064c7d685e9054
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-03-24 21:45:19 +0100

    ŵith back button

--- W03/mySpringBoot/src/main/java/com/example/myspringboot/ConvertController.java
+++ W03/mySpringBoot/src/main/java/com/example/myspringboot/ConvertController.java
@@ -1,6 +1,5 @@
 package com.example.myspringboot;
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ConvertController {
 
     @GetMapping("/convert")
-    public ResponseEntity<String> convert(double inches) {
+    public ResponseEntity<String> convert(int inches) {
         var cm = inches * 2.54;
         var cmPart = (int) cm;
-        var mmPart = (int) cm * 10 % 10;
-        var response = inches + " inches is: " + cmPart + " cm, " + mmPart + " mm.";
+        var mmPart = (int) ((cm * 10) % 10);
+        var response = String.format("""
+                <!DOCTYPE html>
+                <title>Result</title>
+                %d inches is: %d cm, %d mm.
+                <a href="/">Back</a>
+                """, inches, cmPart, mmPart);
         return ResponseEntity.ok(response);
     }
 }
