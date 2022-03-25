7bd9a506491cd64a94111af08df34b102f1cfccc
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-03-25 16:37:40 +0100

    update

--- W03/mySpringBoot/src/main/java/com/example/myspringboot/ConvertController.java
+++ W03/mySpringBoot/src/main/java/com/example/myspringboot/ConvertController.java
@@ -1,6 +1,5 @@
 package com.example.myspringboot;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,10 +8,15 @@ import org.springframework.web.servlet.ModelAndView;
 public class ConvertController {
 
     @GetMapping("/convert")
-    public ModelAndView convert(int inches) {
+    public ModelAndView convert(int inches, int feet) {
         var cm = inches * 2.54;
         var cmPart = (int) cm;
         var mmPart = (int) ((cm * 10) % 10);
+
+        var cmFeet = feet * 3.28;
+        var cmFeetPart = (int) cm;
+        var mmFeetPart = (int) ((cm * 10) % 10);
+
         var result = new ModelAndView("convert");
         result.addObject("inches", inches);
         result.addObject("cmPart", cmPart);
